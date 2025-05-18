76da8c5f2dbad3a15e63205de6b91c054d12f337
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2025-05-18 18:42:15 +0530

    changed domain name

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 //configuring CORS to allow cross-origin requests only from vercel
 const corsOptions = {
     origin: (origin, callback) => {
-        const allowedOrigins = ['https://bug-buster-sigma.vercel.app/', 'http://localhost:5173'];
+        const allowedOrigins = ['https://bug-buster-sigma.vercel.app', 'http://localhost:5173'];
         if (allowedOrigins.includes(origin) || !origin) {
             callback(null, true); // Allow the request
         } else {
